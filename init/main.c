2d72762af5f7ba26f4d0c1866464b758a6efd09a
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-11 16:08:27 -0700

    Added for loop so you are very annoyed

--- init/main.c
+++ init/main.c
@@ -1583,5 +1583,8 @@ static noinline void __init kernel_init_freeable(void)
 	 */
 
 	integrity_load_keys();
-	pr_info("Hi hi hi hi hi sup sup sup am I annoying yet :D");
+	int annoying;
+	for (annoying = 1, annoying = 10, annoying++){
+		pr_info("Hi hi hi hi hi sup sup sup am I annoying yet :D\n");
+	}
 }
